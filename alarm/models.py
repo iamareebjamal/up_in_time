52c39f9e1bc7e732c5654fd8236cf8c3271bb324
iamareebjamal <jamal.areeb@gmail.com>
2016-08-31 15:20:09 +0530

    Added Database for Alarm

--- alarm/models.py
+++ alarm/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Alarm(models.Model):
+	alarm = models.DateTimeField()
+	ip_address = models.GenericIPAddressField()
+
+	def __unicode__(self):
+		return u'Alarm : %s \nIP : %s' % (self.alarm, self.ip_address)
